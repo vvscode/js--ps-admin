90855c3e60c486e2304421c92a7cb6ed28593a75
Vasil Vanchuk <v.vanchuk@tut.by>
2015-10-01 12:17:12 +0300

    Small UI issues

---
+++ app/utils/api.js
@@ -0,0 +1,17 @@
+
+
+var cache = {
+
+};
+
+export default {
+  getResourcesList: function() {
+
+  },
+  getRoutesList: function() {
+
+  },
+  getResourceFieldsList: function(resourceName) {
+
+  }
+};
