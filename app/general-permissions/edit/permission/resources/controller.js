56941203ac6a78c76bd5521c70fe27b9dd7bcffc
Vasil Vanchuk <v.vanchuk@tut.by>
2015-11-11 17:24:16 +0300

    Fix UI/UX issues

--- app/general-permissions/edit/permission/resources/controller.js
+++ app/general-permissions/edit/permission/resources/controller.js
@@ -3,17 +3,17 @@ var get = Ember.get;
 export default Ember.Controller.extend({
 
   onModelChange: function() {
-    if(get(this, 'model.length')) {
       this.send('savePermission');
-    }
   }.observes('model.length', 'model.@each.name', 'model.@each.read.length', 'model.@each.update.length'),
 
   actions: {
-    addResource: function () {
-      get(this, 'model').addObject({
-        name: '',
-        read: [],
-        update: []
+    addResource () {
+      this.API.getDefaultResource().then((name) => {
+        get(this, 'model').addObject({
+          name: name,
+          read: [],
+          update: []
+        });
       });
     }
   }
--- app/general-permissions/edit/permission/route.js
+++ app/general-permissions/edit/permission/route.js
@@ -1,8 +1,14 @@
-var get = Ember.get;
+const { get, setProperties } = Ember;
 
 export default Ember.Route.extend({
   model: function (params) {
-    return this.API.getPermission(params.permission_id);
+    const id = parseInt(params.permission_id);
+    const model = this.modelFor('general-permissions.edit').permissions.findBy('id', id);
+    return this.API.getPermission(id).then((data) => {
+      delete data.id;
+      setProperties(model, data);
+      return model;
+    });
   },
 
   actions: {
