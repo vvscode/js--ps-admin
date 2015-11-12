e4caf860607ed2963a8f48cd96e97a134e348e19
Vasil Vanchuk <v.vanchuk@tut.by>
2015-11-12 16:16:16 +0300

    Use component for pretty display permission settings

---
+++ app/components/permissions-settings-item/component.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+const { get, set, computed } = Ember;
+
+export default Ember.Component.extend({
+  permission: null,
+  action: null,
+
+  checked: computed('permission', 'action', {
+    get(key) {
+      return get(get(this, 'permission'), get(this, 'action'));
+    },
+    set(key, value){
+      set(get(this, 'permission'), get(this, 'action'), value);
+      return value;
+    }
+  }),
+
+  forcedFalse: computed('permission', 'action', function() {
+    const fieldName = `${get(this, 'action')}_forced`;
+    return get(get(this, 'permission'), fieldName) === -1;
+  }),
+
+  forcedTrue: computed('permission', 'action', function() {
+    const fieldName = `${get(this, 'action')}_forced`;
+    return get(get(this, 'permission'), fieldName) === 1;
+  })
+});
